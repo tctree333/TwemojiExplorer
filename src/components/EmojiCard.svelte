bf6ba4c8381af6b5900006e1dc751691fc17ec69
Tomi Chen <tctree333@users.noreply.github.com>
2021-06-07 15:25:06 -0700

    add copy buttons

--- src/components/EmojiCard.svelte
+++ src/components/EmojiCard.svelte
@@ -1,30 +1,119 @@
 <script lang="ts">
+  import { onDestroy } from 'svelte';
+
   import type { EmojiData } from '../lib/types';
 
   export let emoji: EmojiData;
+
+  let status: boolean = null;
+  let timeoutHandle: NodeJS.Timeout;
+
+  function fail() {
+    status = false;
+    timeoutHandle = setTimeout(() => {
+      status = null;
+    }, 5000);
+  }
+
+  function copySvg() {
+    fetch(emoji.path)
+      .then((res) => {
+        if (res.ok) {
+          res
+            .text()
+            .then((data) => {
+              navigator.clipboard
+                .writeText(data)
+                .then(() => {
+                  status = true;
+                  timeoutHandle = setTimeout(() => {
+                    status = null;
+                  }, 5000);
+                })
+                .catch(fail);
+            })
+            .catch(fail);
+        }
+      })
+      .catch(fail);
+  }
+  function copyCodepoint() {
+    navigator.clipboard
+      .writeText(emoji.codepoint)
+      .then(() => {
+        status = true;
+        timeoutHandle = setTimeout(() => {
+          status = null;
+        }, 5000);
+      })
+      .catch(fail);
+  }
+  onDestroy(() => {
+    clearTimeout(timeoutHandle);
+  });
 </script>
 
 <style>
   div {
+    position: relative;
+
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    height: 148px;
+    padding: 0.75rem;
+    padding-top: 1rem;
+
     text-align: center;
-
-    height: 152px;
-    padding: 0.5rem;
-
-    background-color: hsl(0deg, 0%, 90%);
+    background-color: hsl(0, 0%, 90%);
     border-radius: 0.5rem;
   }
   img {
     display: block;
+    margin-bottom: 1rem;
+  }
+  p {
+    font-size: 0.875rem;
+  }
+  div.actions {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    display: none;
+    background-color: transparent;
+  }
+  div.wrapper:hover div.actions {
+    display: block;
+  }
+  button {
+    margin-bottom: 0.5rem;
+  }
+  button,
+  a {
+    display: block;
+    width: 100%;
+
+    padding: 0.375rem;
+
+    font-size: 0.875rem;
+    background-color: hsl(0, 0%, 100%);
+    border-radius: 0.4rem;
   }
 </style>
 
-<div>
-  <a href="https://emojipedia.org/{emoji.slug.replace(/_/g, '-')}"
-    ><img alt={emoji.emoji} src={emoji.path} width="36" height="36" loading="lazy" /></a>
-  <p>{emoji.codepoint}</p>
+<div
+  class="wrapper"
+  style={status !== null ? `background-color: ${status ? 'hsl(110, 66%, 81%)' : 'hsl(0, 62%, 90%)'}` : ''}>
+  <img alt={emoji.emoji} src={emoji.path} width="42" height="42" loading="lazy" />
+  <p>{emoji.slug.replace(/_/g, ' ')}</p>
+  <div class="actions">
+    <button on:click={copySvg}>Copy SVG</button>
+    <button on:click={copyCodepoint}>Copy Codepoint</button>
+    <a href="https://emojipedia.org/{emoji.slug.replace(/_/g, '-')}">Emojipedia</a>
+  </div>
 </div>
