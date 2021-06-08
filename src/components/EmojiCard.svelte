614f3df39ae4d4daf379f906ead508cc64e54059
Tomi Chen <tctree333@users.noreply.github.com>
2021-06-08 00:34:41 -0700

    don't show all cards

--- src/components/EmojiCard.svelte
+++ src/components/EmojiCard.svelte
@@ -79,7 +79,6 @@
     padding: 0.75rem;
     padding-top: 1rem;
 
-    text-align: center;
     background-color: hsl(0, 0%, 99%);
     border: 1px solid hsl(0, 0%, 70%);
     border-radius: 0.5rem;
--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -4,6 +4,15 @@
 
   export let title: string;
   export let emojis: EmojiData[];
+
+  let showDefault = 23;
+  let showAll = false;
+
+  $: {
+    if (emojis) {
+      showAll = false;
+    }
+  }
 </script>
 
 <style>
@@ -25,13 +34,54 @@
     grid-template-columns: repeat(auto-fill, minmax(148px, 1fr));
     gap: 16px;
   }
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 148px;
+    padding: 0.75rem;
+
+    background-color: hsl(184, 88%, 90%);
+    border: 1px solid hsl(213, 45%, 77%);
+    border-radius: 0.5rem;
+
+    font-size: 1.125rem;
+  }
+  svg {
+    display: inline-block;
+    margin-left: 0.2em;
+  }
 </style>
 
 <section>
   <h2>{title} ({emojis.length})</h2>
   <div class="grid">
-    {#each emojis as emoji}
+    {#each emojis.slice(0, showDefault) as emoji}
       <Card {emoji} />
     {/each}
+    {#if emojis.slice(showDefault).length > 0}
+      {#if showAll}
+        {#each emojis.slice(showDefault) as emoji}
+          <Card {emoji} />
+        {/each}
+      {:else}
+        <button
+          on:click={() => {
+            showAll = true;
+          }}>
+          <p>Show All</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+          </svg>
+        </button>
+      {/if}
+    {/if}
   </div>
 </section>
